bdfd82df48b6ba31a060cc1f0fe3320d1ab7b4b4
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2026-06-20 15:37:20 -0500

    feat: implementar capa de servicio para firmado asimetrico RS256

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -7,13 +7,33 @@ export class JwtService {
      * @param {Object} payload - Los datos del usuario a incluir en el token.
      * @returns {string} El token JWT generado.
      */
-    static signToken(payload) {
-        // TODO: Implementar lógica de firmado.
-        // 1. Verificar si config.ALGORITHM es 'RS256' o 'HS256'.
-        // 2. Si es 'RS256', usar config.PRIVATE_KEY.
-        // 3. Si es 'HS256', usar config.JWT_SECRET.
-        // 4. Establecer un tiempo de expiración (ej. 1h).
-        // 5. Retornar el token firmado usando jwt.sign().
+     static signToken(user) {
+
+        const payload = {
+            sub: user.id,
+            name: user.name
+        };
+
+        if (config.ALGORITHM === 'RS256') {
+
+            return jwt.sign(
+                payload,
+                config.PRIVATE_KEY,
+                {
+                    algorithm: 'RS256',
+                    expiresIn: '1m'
+                }
+            );
+        }
+
+        return jwt.sign(
+            payload,
+            config.JWT_SECRET,
+            {
+                algorithm: 'HS256',
+                expiresIn: '1m'
+            }
+        );
     }
 
     /**
@@ -21,12 +41,32 @@ export class JwtService {
      * @param {string} token - El token JWT a verificar.
      * @returns {Object|null} El payload decodificado o null si es inválido.
      */
-    static verifyToken(token) {
-        // TODO: Implementar lógica de verificación.
-        // 1. Verificar si config.ALGORITHM es 'RS256' o 'HS256'.
-        // 2. Si es 'RS256', usar config.PUBLIC_KEY para verificar.
-        // 3. Si es 'HS256', usar config.JWT_SECRET para verificar.
-        // 4. Retornar el payload decodificado usando jwt.verify().
-        // 5. Manejar posibles errores (token expirado, firma inválida) y retornar null.
+       static verifyToken(token) {
+
+        try {
+
+            if (config.ALGORITHM === 'RS256') {
+
+                return jwt.verify(
+                    token,
+                    config.PUBLIC_KEY,
+                    {
+                        algorithms: ['RS256']
+                    }
+                );
+            }
+
+            return jwt.verify(
+                token,
+                config.JWT_SECRET,
+                {
+                    algorithms: ['HS256']
+                }
+            );
+
+        } catch (error) {
+
+            throw error;
+        }
     }
 }
